3b6a5be7cba2ae249b7971d136f3e69544820ce5
hugodemascarel <72524914+hugodemascarel@users.noreply.github.com>
2020-11-19 18:23:52 +0100

    Update mainwindow.h

--- mainwindow.h
+++ mainwindow.h
@@ -3,6 +3,7 @@
 
 #include <QMainWindow>
 #include <QApplication>
+#include <QPushButton>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -11,13 +12,14 @@ QT_END_NAMESPACE
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
-
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
-
+private slots:
+    void handleButton();
 private:
     Ui::MainWindow *ui;
+    QPushButton *solve_button;
 };
 #endif // MAINWINDOW_H
 
