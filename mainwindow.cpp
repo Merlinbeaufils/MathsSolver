5923bd54538d98b4f3e0426b4d97890469c3c5f3
hugodemascarel <72524914+hugodemascarel@users.noreply.github.com>
2020-11-19 18:22:27 +0100

    Update mainwindow.cpp

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,10 +1,14 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QCoreApplication>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
+    solve_button = new QPushButton("SOLVE", this);
+    solve_button -> setGeometry(QRect(QPoint(50,50), QSize(300,100)));
+    connect(solve_button, SIGNAL(released()), this, SLOT(handleButton()));
     ui->setupUi(this);
 }
 
@@ -13,3 +17,7 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+void MainWindow::handleButton(){
+    solve_button->setText("SOLVED");
+    solve_button->resize(100,100);
+}
